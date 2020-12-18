8c70514b114e6c5bf49f4de4bf50bb525524893c
Martin <martin.grescu@gmail.com>
2020-12-18 17:00:51 +0100

    Ajout  TODO

--- src/main/java/com/payline/payment/wechatpay/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/payline/payment/wechatpay/service/impl/PaymentServiceImpl.java
@@ -2,7 +2,6 @@ package com.payline.payment.wechatpay.service.impl;
 
 import com.payline.payment.wechatpay.bean.configuration.RequestConfiguration;
 import com.payline.payment.wechatpay.bean.nested.SignType;
-import com.payline.payment.wechatpay.bean.nested.TradeType;
 import com.payline.payment.wechatpay.bean.request.UnifiedOrderRequest;
 import com.payline.payment.wechatpay.bean.response.UnifiedOrderResponse;
 import com.payline.payment.wechatpay.exception.PluginException;
@@ -40,7 +39,7 @@ public class PaymentServiceImpl implements PaymentService {
                     .totalFee(paymentRequest.getAmount().getAmountInSmallestUnit().toString())
                     .spBillCreateIp(paymentRequest.getBrowser().getIp())
                     .notifyUrl(configuration.getEnvironment().getNotificationURL())
-                    .tradeType(TradeType.NATIVE)
+                    .tradeType("NATIVE") // TODO: Utiliser le TradeType
                     .productId(paymentRequest.getOrder().getReference())
                     .appId(configuration.getPartnerConfiguration().getProperty(PartnerConfigurationKeys.APPID))
                     .merchantId(configuration.getContractConfiguration().getProperty(ContractConfigurationKeys.MERCHANT_ID).getValue())
